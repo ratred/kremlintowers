c9ad7f02f0e05b5fd210100fd53824f0de642c1e
Oleg <oleg@ledorub.org>
2018-04-28 22:36:40 +0300

    Using tiles map

---
+++ assets/kremlintowers.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="kremlintowers" tilewidth="32" tileheight="64" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="2" type="wall_top">
+  <image width="32" height="32" source="wall.png"/>
+ </tile>
+ <tile id="3" type="wall_bottom">
+  <image width="32" height="32" source="downwall.png"/>
+ </tile>
+ <tile id="4" type="player1_tower">
+  <image width="32" height="64" source="tower.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="64" source="tower_selected.png"/>
+ </tile>
+ <tile id="6" type="ground">
+  <image width="32" height="32" source="ground.png"/>
+ </tile>
+ <tile id="7" type="player2_tower">
+  <image width="32" height="64" source="tower.png"/>
+ </tile>
+</tileset>
